34766400d17820e04dd2c4b3e44a5489bab1f705
Thomas Joseph <joevad@gmail.com>
2014-08-10 22:59:55 +0530

    Plot4 R code for committing

--- Plot4.R
+++ Plot4.R
@@ -12,7 +12,9 @@ seldata$date <- weekdays(as.Date(seldata$date,"%d/%m/%Y"))
 ## Plot the data
 
 par(mfrow = c(2,2))
-with(seldata,plot(time,active))
-with(seldata,plot(time,voltage))
-with(seldata,plot(time,c(SM1,SM2,SM3))
-with(seldata, plot(time,Reactive))
+with(seldata,plot(time,active,ylab = "Global Active Power"))
+with(seldata,plot(time,voltage, ylab = "Voltage"))
+with(seldata,plot(time,SM1,ylab = "Energy Sub Metering"))
+with(seldata, plot(time,Reactive,ylab = "Global_Reactive_Power"))
+dev.copy(png,file = "plot4.png")
+dev.off()
