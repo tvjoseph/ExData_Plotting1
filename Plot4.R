716fa7f2c8e19a33047db7ee27fc6b57cea3855a
Thomas V Joseph <tvjoseph@users.noreply.github.com>
2014-08-10 21:44:32 +0530

    Create Plot4.R

---
+++ Plot4.R
@@ -0,0 +1,18 @@
+## Read the data into a variable called housedat
+housedat <- read.table("household_power_consumption.txt",header = T, sep = ";",na.string = "?")
+## Rename the columns of housedat
+names(housedat) <- c("date","time","active","Reactive","voltage","intensity","SM1","SM2","SM3")
+## Coerse the dates in the proper format
+housedat$date <- as.Date(housedat$date,format = "%d/%m/%Y")
+## Filter the data as per the dates
+seldata <- housedat[housedat$date >= "2007-02-01" & housedat$date <= "2007-02-02",]
+## convert the dates into week days
+seldata$date <- weekdays(as.Date(seldata$date,"%d/%m/%Y"))
+
+## Plot the data
+
+par(mfrow = c(2,2))
+with(seldata,plot(time,active))
+with(seldata,plot(time,voltage))
+with(seldata,plot(time,c(SM1,SM2,SM3))
+with(seldata, plot(time,Reactive))
