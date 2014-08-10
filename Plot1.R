51b59ce0f58a2e2f964b689f3e53f8ec67227167
Thomas V Joseph <tvjoseph@users.noreply.github.com>
2014-08-10 20:33:03 +0530

    Create Plot1.R

---
+++ Plot1.R
@@ -0,0 +1,9 @@
+## This is the command for loading the file
+housedat <- read.table("household_power_consumption.txt",header = T, sep = ";",na.string = "?")
+names(housedat) <- c("date","time","active","Reactive","voltage","intensity","SM1","SM2","SM3")
+housedat$date <- as.Date(housedat$date,format = "%d/%m/%Y")
+seldata <- housedat[housedat$date >= "2007-02-01" & housedat$date <= "2007-02-02",]
+
+hist(seldata$active,col = "red",main = "Global Active Power",xlab = "Global Active Power(kilowatts)", ylab = "Frequency")
+dev.copy(png,file = "plot1.png")
+dev.off()
